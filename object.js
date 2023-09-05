97adf97f99dce809681961cfba4fb8d5ff0a125e
fardinalam <fardinalam2020@gmail.com>
2023-09-05 19:33:57 +0600

    first file update

--- object.js
+++ object.js
@@ -1,5 +1,11 @@
 // create object using object literals
 const player = {};
+// objects
 player.name = 'Small Nirob';
-player.speciality
-
+player.speciality = 'batsman';
+// methods
+player.bat = function(){
+    console.log('swing your bat');
+}
+console.log(player);
+player.bat();
