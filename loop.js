690a331352b0e2a1a522b9847de22dafaac33898
fardinalam <fardinalam2020@gmail.com>
2024-02-16 16:33:05 +0600

    new added

---
+++ loop.js
@@ -0,0 +1,7 @@
+const numbers = [12, 34 , 43, 54, 32];
+// for (number of numbers){
+//     console.log(number)
+// }
+
+// 1. For of loop can not be used with objects
+const bottle = { color: 'yellow', price: 50, isCleaned: true, capacity: 1};
