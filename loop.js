3ca79db770d794c4bf4c17dd8e3efed61a7fd71b
fardinalam <fardinalam2020@gmail.com>
2024-02-26 18:49:30 +0600

    for of, for in

--- loop.js
+++ loop.js
@@ -2,6 +2,26 @@ const numbers = [12, 34 , 43, 54, 32];
 // for (number of numbers){
 //     console.log(number)
 // }
-
 // 1. For of loop can not be used with objects
 const bottle = { color: 'yellow', price: 50, isCleaned: true, capacity: 1};
+// First option to loop through an array
+const keys = Object.keys(bottle);
+// console.log(keys);
+/*
+3 ways to read object properties
+bottle.color
+bottle['color']
+bootle[key]
+ */
+for(const key of keys){
+    // console.log(key, bottle[key]);
+}
+
+// for in loop
+for( const key in bottle){
+    console.log(key, bottle[key]);
+}
+
+// advanced
+const pair = Object.entries(bottle);
+console.log(pair)
